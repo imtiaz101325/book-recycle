db3d31b6a2f005a0e56bffba5ef646db29860dc2
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2022-05-18 16:54:14 +0200

    Added authentication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { DataStore } from "@aws-amplify/datastore";
 import { useEffect } from "react";
+import { Authenticator, View } from "@aws-amplify/ui-react";
 
 import { Book } from "./models";
 
@@ -13,7 +14,16 @@ function App() {
     data();
   });
 
-  return <div></div>;
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <View>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </View>
+      )}
+    </Authenticator>
+  );
 }
 
 export default App;
