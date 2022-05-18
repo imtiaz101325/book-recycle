50260a71e85fc6d291350483a49f0fd4898726e1
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2022-05-18 15:49:11 +0200

    Basic query

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { DataStore } from "@aws-amplify/datastore";
+import { useEffect } from "react";
+
+import { Book } from "./models";
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+  useEffect(() => {
+    async function data() {
+      const models = await DataStore.query(Book);
+      console.log(models);
+    }
+
+    data();
+  });
+
+  return <div></div>;
 }
 
 export default App;
